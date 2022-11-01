773a0dff00bc6159c36cebf1fe7d626497933705
tztx2004 <tztx2004@gmail.com>
2022-11-01 16:17:25 +0900

    updated

--- practice3.py
+++ practice3.py
@@ -315,25 +315,49 @@
 # print(score_file.readline())
 # score_file.close()
 
-score_file = open("score.txt", "r", encoding="utf8")
-while True:
-    line = score_file.readline()
-    if not line:
-        break
-    print(line, end="")
-score_file.close()
+# score_file = open("score.txt", "r", encoding="utf8")
+# while True:
+#     line = score_file.readline()
+#     if not line:
+#         break
+#     print(line, end="")
+# score_file.close()
 
-score_file = open("score.txt", "r", encoding="utf8")
-lines = score_file.readlines() # list 형태로 저장
-for line in lines:
-    print(line, end="")
-score_file.close()
+# score_file = open("score.txt", "r", encoding="utf8")
+# lines = score_file.readlines() # list 형태로 저장
+# for line in lines:
+#     print(line, end="")
+# score_file.close()
 
-import pickle
-profile_file = open("profile.pickle", "wb")
-profile = {"이름":"박명수", "나이":30, "취미":["축구", "골프", "코딩"]}
-print(profile)
-pickle.dump(profile, profile_file) # profile에 있는 정보를 file에 저장
-profile_file.close()
+# import pickle
+# profile_file = open("profile.pickle", "wb")
+# profile = {"이름":"박명수", "나이":30, "취미":["축구", "골프", "코딩"]}
+# print(profile)
+# pickle.dump(profile, profile_file) # profile에 있는 정보를 file에 저장
+# profile_file.close()
 
-print("updated!")
+# profile_file = open("profill.pickle", "rb")
+# profile = pickle.load(profile_file)
+# print(profile)
+# profile_file.close()
+
+# # with
+# import pickle
+# with open("profile.pickle", "rb") as profile_file:
+#     print(pickle.load(profile_file))
+
+# with open("study.txt", "w", encoding="utf8") as study_file:
+#     study_file.write("파이썬을 열심히 공부하고 있어요")
+
+# with open("study.txt", "r", encoding="utf8") as study_file:
+#     print(study_file.read())
+
+# 퀴즈 7
+for i in range(1,51):
+    with open(str(i) + "주차.txt","w",encoding="utf8") as report_file:
+        report_file.write("- {0} 주차 주간보고 -".format(i))
+        report_file.write("\n부서 : ")
+        report_file.write("\n이름 : ")
+        report_file.write("\n업무 요약 : ")
+
+
